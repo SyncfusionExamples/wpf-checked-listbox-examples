80e5673aa99c92f83ae6e3f30472d94c96fde59d
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-03-12 14:12:31 +0530

    CheckListBox ug samples committed.

---
+++ CheckListBox-Apperance/ViewModel/ViewModel.cs
@@ -0,0 +1,97 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CheckListBox_Apperance
+{
+    class ViewModel : NotificationObject
+    {
+        private Brush foregroundBrush = Brushes.Red;
+        private Brush backgroundBrush = Brushes.SkyBlue;
+        private Brush mouseOverBackgroundBrush = Brushes.Pink;
+        private Brush selectedItemBackground = Brushes.Yellow;
+        private ObservableCollection<string> daysCollection = new ObservableCollection<string>();
+
+        public ObservableCollection<string> DaysCollection
+        {
+            get
+            {
+                return daysCollection;
+            }
+            set
+            {
+                daysCollection = value;
+                this.RaisePropertyChanged("DaysCollection");
+            }
+        }
+        
+        public Brush ForegroundBrush
+        {
+            get
+            {
+                return foregroundBrush;
+            }
+            set
+            {
+                foregroundBrush = value;
+                this.RaisePropertyChanged("ForegroundBrush");
+            }
+        }
+
+        public Brush BackgroundBrush
+        {
+            get
+            {
+                return backgroundBrush;
+            }
+            set
+            {
+                backgroundBrush = value;
+                this.RaisePropertyChanged("BackgroundBrush");
+            }
+        }
+        public Brush MouseOverBackgroundBrush
+        {
+            get
+            {
+                return mouseOverBackgroundBrush;
+            }
+            set
+            {
+                mouseOverBackgroundBrush = value;
+                this.RaisePropertyChanged("MouseOverBackgroundBrush");
+            }
+        }
+        
+        public Brush SelectedItemBackground
+        {
+            get
+            {
+                return selectedItemBackground;
+            }
+            set
+            {
+                selectedItemBackground = value;
+                this.RaisePropertyChanged("SelectedItemBackground");
+            }
+        }
+
+        public ViewModel()
+        {
+            //Days added in the collection
+            DaysCollection.Add("Sunday");
+            DaysCollection.Add("Monday");
+            DaysCollection.Add("Tuesday");
+            DaysCollection.Add("Wednesday");
+            DaysCollection.Add("Thursday");
+            DaysCollection.Add("Friday");
+            DaysCollection.Add("Saturday");
+        }
+
+    }
+}
---
+++ CheckListBox-Themes/ViewModel/VIewModel.cs
@@ -0,0 +1,103 @@
+﻿using Syncfusion.SfSkinManager;
+using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CheckListBox_Themes
+{
+    public class ViewModel : NotificationObject
+    {
+        private ObservableCollection<string> themes = new ObservableCollection<string>();
+        private ObservableCollection<string> daysCollection = new ObservableCollection<string>();
+        private ICommand selectionChangedCommand;
+
+        public ICommand SelectionChangedCommand
+        {
+            get
+            {
+                return selectionChangedCommand;
+            }
+        }
+
+        public ObservableCollection<string> Themes
+        {
+            get
+            {
+                return themes;
+            }
+            set
+            {
+                themes = value;
+                this.RaisePropertyChanged("Themes");
+            }
+        }
+
+        public ObservableCollection<string> DaysCollection
+        {
+            get
+            {
+                return daysCollection;
+            }
+            set
+            {
+                daysCollection = value;
+                this.RaisePropertyChanged("DaysCollection");
+            }
+        }
+        public ViewModel()
+        {
+            //Days added in the collection
+            DaysCollection.Add("Sunday");
+            DaysCollection.Add("Monday");
+            DaysCollection.Add("Tuesday");
+            DaysCollection.Add("Wednesday");
+            DaysCollection.Add("Thursday");
+            DaysCollection.Add("Friday");
+            DaysCollection.Add("Saturday");
+
+            //Theme list  added in the collection
+            Themes.Add("Blend");
+            Themes.Add("Lime");
+            Themes.Add("Metro");
+            Themes.Add("Office2010Black");
+            Themes.Add("Office2010Blue");
+            Themes.Add("Office2010Silver");
+            Themes.Add("Office2013DarkGray");
+            Themes.Add("Office2013LightGray");
+            Themes.Add("Office2013White");
+            Themes.Add("Office2016Colorful");
+            Themes.Add("Office2016DarkGray");
+            Themes.Add("Office2016White");
+            Themes.Add("Office365");
+            Themes.Add("Saffron");
+            Themes.Add("VisualStudio2013");
+            Themes.Add("VisualStudio2015");
+            selectionChangedCommand = new DelegateCommand<object>(selectionChanged);
+        }
+
+        public void selectionChanged(object parameter)
+        {
+            WindowCollection windows = Application.Current.Windows;
+            if (windows.Count > 0)
+            {
+                Window samplewindow = windows[0];
+                ComboBox combo = parameter as ComboBox;
+                if (combo != null)
+                {
+                    if (combo.SelectedItem != null)
+                    {
+                        string themename = combo.SelectedValue.ToString();
+                        SfSkinManager.SetVisualStyle(samplewindow, (VisualStyles)Enum.Parse(typeof(VisualStyles), themename));
+                    }
+                }
+            }
+        }
+    }
+}
