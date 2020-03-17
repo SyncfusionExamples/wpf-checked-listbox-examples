a9d0426f1138cc86fd5d5a5433e830256a149589
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-03-17 17:52:53 +0530

    Virtualization sample committed.

---
+++ Samples/Virtualization/ViewModel/ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace CheckListBox_Virtualization
+{
+    public class ViewModel
+    {
+        private ObservableCollection<Model> collection = new ObservableCollection<Model>();
+        public ObservableCollection<Model> Collection
+        {
+            get { return collection; }
+            set { collection = value; }
+        }
+
+        public ICommand LoadedCommand { get; set; }
+        public void OnLoaded(object param)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(Collection);
+
+            //Adding group description
+            view.GroupDescriptions.Add(new PropertyGroupDescription("GroupName"));
+        }
+
+        public ViewModel()
+        {
+            Collection = new ObservableCollection<Model>();
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Model myitem = new Model() { Name = "Module " + i.ToString(), GroupName = "Group" + j.ToString() };
+                    Collection.Add(myitem);
+                }
+            }
+
+            //Initialize the checklistbox LoadedCommand
+            LoadedCommand = new DelegateCommand<object>(OnLoaded);
+
+        }
+    }
+}
