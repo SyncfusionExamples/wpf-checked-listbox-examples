88a5b5ff994093bb725f5d6b6d0d7ff028a4ac5e
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-03-18 17:19:54 +0530

    Check items using SelectItem collection sample added.

---
+++ Samples/CheckItems/Model/Vegetable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckListBox_CheckItem
+{
+    class Vegetable
+    {
+        public string Category { get; set; }
+        public string Price { get; set; }
+        public string Name { get; set; }
+    }
+}
