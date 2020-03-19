f9cc49eab275d3e3c93d56d279bedd56cc39389e
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-03-19 12:53:28 +0530

    Templates sample committed.

---
+++ Samples/Templates/ViewModel/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace CheckListBox_Template
+{
+    class ViewModel {
+        public ObservableCollection<Vegetable> Vegetables { get; set; }
+        public ICommand LoadedCommand { get; set; }
+        public void OnLoaded(object param) {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(Vegetables);
+
+            //Adding group description
+            view.GroupDescriptions.Add(new PropertyGroupDescription("Category"));
+        }
+        public ViewModel() {
+            Vegetables = new ObservableCollection<Vegetable>();
+            Vegetables.Add(new Vegetable { Price = "$10", Name = "Yarrow", Category = "Leafy and Salad" });
+            Vegetables.Add(new Vegetable { Price = "$20", Name = "Pumpkins", Category = "Leafy and Salad" });
+            Vegetables.Add(new Vegetable { Price = "$30", Name = "Cabbage", Category = "Leafy and Salad" });
+            Vegetables.Add(new Vegetable { Price = "$10", Name = "Spinach", Category = "Leafy and Salad" });
+            Vegetables.Add(new Vegetable { Price = "$20", Name = "Wheat Grass", Category = "Leafy and Salad" });
+            Vegetables.Add(new Vegetable { Price = "$30", Name = "Horse gram", Category = "Beans" });
+            Vegetables.Add(new Vegetable { Price = "$10", Name = "Chickpea", Category = "Beans" });
+            Vegetables.Add(new Vegetable { Price = "$20", Name = "Green bean", Category = "Beans" });
+            Vegetables.Add(new Vegetable { Price = "$30", Name = "Garlic", Category = "Bulb and Stem" });
+            Vegetables.Add(new Vegetable { Price = "$10", Name = "Onion", Category = "Bulb and Stem" });
+            Vegetables.Add(new Vegetable { Price = "$20", Name = "Nopal", Category = "Bulb and Stem" });
+
+            //Initialize the checklistbox LoadedCommand
+            LoadedCommand = new DelegateCommand<object>(OnLoaded);
+        }
+    }
+}
