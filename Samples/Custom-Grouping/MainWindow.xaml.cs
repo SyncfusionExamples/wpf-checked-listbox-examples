d65f4ee7026510f61ae6d3739764dc2f7c7b37f2
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-04-01 13:06:51 +0530

    custom grouping sample committed

--- Samples/Custom-Grouping/MainWindow.xaml.cs
+++ Samples/Custom-Grouping/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Syncfusion.Windows.Tools.Controls;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
--- Samples/Custom-Grouping/Model/Model.cs
+++ Samples/Custom-Grouping/Model/Model.cs
@@ -2,7 +2,8 @@
 
 namespace Custom_Grouping
 {
-    public class Employee {
+    public class Employee
+    {
         public string Name { get; set; }
         public int Age { get; set; }
         public DateTime DOB { get; set; }
